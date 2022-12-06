e37cab1b4fef3a1783f6167a7e25862d248783c7
avalasiuk <avalasiuk@kontur.io>
2022-12-06 18:56:05 +0300

    Add empty install.sh file to keep compatibility with geocint-runner

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add here your custom intallation instructions
+# Please, use sudo to run commands and -y for apt
+# sudo apt install -y osmium-tool
+
+# remove this row afret adding any row with installation
+exit 0
