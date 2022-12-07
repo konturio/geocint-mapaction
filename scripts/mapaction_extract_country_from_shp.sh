f48f6895e6c0d2f2eb64225ca37c3806e65370c3
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-12-07 11:37:43 +0300

    add script that builds CKAN dataset description

--- scripts/mapaction_extract_country_from_shp.sh
+++ scripts/mapaction_extract_country_from_shp.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+set -e
+
 country_geojson_filename=$(basename "$1")
 country_code="${country_geojson_filename%%.*}"
 
 input_shp_name=$2
-output_shp_name="${3//"{country_code}"/$country_code}"
+output_name="${3//"{country_code}"/$country_code}"
+output_shp_name="$output_name.shp"
 
 echo "country_geojson_filename $country_geojson_filename"
 echo "input_shp_name $input_shp_name"
 echo "output_shp_name $output_shp_name"
 
 mkdir -p "$(dirname $output_shp_name)"
-
 ogr2ogr -clipsrc static_data/countries/$country_geojson_filename -lco ENCODING=UTF8 -skipfailures $output_shp_name $input_shp_name
+
+output_zip_path="data/out/country_extractions/$(basename $output_name).zip"
+zip -r -j $output_zip_path "$(dirname $output_shp_name)"
+
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp $output_zip_path s3://mekillot-backet/akalenik/"$(basename $output_zip_path)"
