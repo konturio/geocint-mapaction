f74c427737c2c757b2e3901f748e949c536e65db
lgudyma <lgudyma@kontur.io>
2022-12-08 22:44:00 +0200

    13288-create-dataset-export-per-country-both-json-and-shp

--- scripts/mapaction_export.sh
+++ scripts/mapaction_export.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
-PSQL_SELECT='psql -q -t -U gis -c'
-OUTDIR=data/out/mapaction/
+#!/bin/bash
 
-layers=$(${PSQL_SELECT} "SELECT distinct ma_theme FROM mapaction_data;")
+OUTDIR=data/out/mapaction
 
-for layer in ${layers}; do
-    ogr2ogr -f "ESRI Shapefile" ${OUTDIR}map_action_${layer}.shp PG:"dbname=gis" -sql "SELECT * FROM mapaction_data WHERE ma_theme = '${layer}'"
-    ogr2ogr -f "GeoJSON" ${OUTDIR}map_action_${layer}.geojson PG:"dbname=gis" -sql "SELECT * FROM mapaction_data WHERE ma_theme = '${layer}'"
-done
+base_name=$(basename "$1")
+prefix_name=${base_name%.*}
+#changing prefix osm_ to mapaction_
+#example "osm_tanzania" to "mapaction_tanzania"
+mapaction_table_name=${prefix_name/osm_/mapcation_}
 
+# generate layernames for export
+# MapAction naming convention 
+# geoextent_category_theme_geometry_scale_source_permission[_FreeText]
+# geoextent - country_code (tags ->> 'ISO3166-1:alpha3')
+# category_theme_geometry - columnms in mapaction table
+# scale - s0-3 -for country it is s0
+# source - 'osm'
+# permission - mm mapaction Only
+while IFS="," read -r country_code ma_category ma_theme feature_type ma_tag
+do
+    # if ma_tag is empty discard last "_" and convert name to lowercase
+    output=$(echo "${country_code}_${ma_category}_${ma_theme}_${feature_type}_s0_osm_mm_${ma_tag}" | sed 's/_$//g' | sed -e 's/\(.*\)/\L\1/')
+    # generate sql string for ogr, if there are additional columns for layer
+    sql=$(psql -t -A -F , -c "select mapaction_data_export('${mapaction_table_name}', '${country_code}', '${ma_category}', '${ma_theme}', '${ma_tag}', '${feature_type}');")
+    ogr2ogr -f "GeoJSON" $OUTDIR$output.geojson PG:"dbname=gis" -sql "${sql}"
+    ogr2ogr -f "ESRI Shapefile" $OUTDIR$output.shp PG:"dbname=gis" -sql "${sql}"
+done < <( psql -t -A -F , -c "SELECT country_code, ma_category, ma_theme, feature_type, ma_tag FROM ${mapaction_table_name} group by 1,2,3,4,5")
--- scripts/mapaction_osm_data_filler.sh
+++ scripts/mapaction_osm_data_filler.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 base_name=$(basename "$1")
-#example "osm_tanzatia"
 prefix_name=${base_name%.*} 
 #changing prefix osm_ to mapaction_
+#example "osm_tanzania" to "mapaction_tanzania"
 mapaction_table_name=${prefix_name/osm_/mapcation_} 
 psql -f tables/mapaction_data_table.sql -v tablename=${prefix_name}
 psql -f tables/mapaction_data_filler.sql -v osm_table=${prefix_name} -v ma_table=${mapaction_table_name}
