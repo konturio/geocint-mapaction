209841d824f22e222e21999d064048d10df42a32
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-12-14 23:39:38 +0300

    output separate shp and geojson archives for cmf

--- scripts/mapaction_upload_cmf.sh
+++ scripts/mapaction_upload_cmf.sh
@@ -7,6 +7,8 @@ country_code=$(basename "$country_extraction_path")
 echo "country_extraction_path $country_extraction_path"
 echo "country_code $country_code"
 
-(cd $country_extraction_path; zip -r "../../cmf/$country_code.zip" .)
+(cd $country_extraction_path; zip -r "../../cmf/$country_code.shp.zip" . -x \*.geojson)
+(cd $country_extraction_path; zip -r "../../cmf/$country_code.geojson.zip" . -i \*.geojson)
 
-aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.zip"
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.shp.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.shp.zip"
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.geojson.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.geojson.zip"
