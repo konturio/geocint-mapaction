eda1cc086fe7873fb5ace1c4830880554f6a58bb
lgudyma <lgudyma@kontur.io>
2022-12-16 20:01:09 +0200

    testing ckat. TODO: remove api keys

--- scripts/mapaction_upload_cmf.sh
+++ scripts/mapaction_upload_cmf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: DELETE
+CKAN_API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzEyMTM0OTIsImp0aSI6IkM1ZzBFNUEybEppakJmeEpmYks2eUhoQi04RmwyYTNObFBydWFJQ21RWjFSMjhmM3JQNnprRzdWU0lqY25pVWVPOVNNaVpoWERRNUN3SE8wIn0.PtEaPRU3vEl2lbySff--e8TUjPk3kHZxxzp5M0UhEy0
+CKAN_BASE_URL=https://geocint.kontur.io/ckan
 # this script accepts path of country cmf directory as first argument,
 # creates zip archives for shp and geojson content and uploads them in s3
 # then scripts/build_ckan_cmf_description.py is used to generate ckan
@@ -16,8 +19,8 @@ echo "country_code $country_code"
 (cd $country_extraction_path; zip -r "../../cmf/$country_code.geojson.zip" . -i \*.geojson)
 
 # FIXME: s3 bucket name and endpoint url should be specified in env variables
-aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.shp.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.shp.zip"
-aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.geojson.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.geojson.zip"
+#aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.shp.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.shp.zip"
+#aws s3 --endpoint-url=https://storage.yandexcloud.net cp "data/out/cmf/$country_code.geojson.zip" "s3://mekillot-backet/akalenik/cmf/$country_code.geojson.zip"
 
 aws s3 cp "data/out/cmf/$country_code.shp.zip" s3://geodata-eu-central-1-kontur-public/mapaction_dataset/$country_code.shp.zip --acl public-read
 aws s3 cp "data/out/cmf/$country_code.geojson.zip" s3://geodata-eu-central-1-kontur-public/mapaction_dataset/$country_code.geojson.zip --acl public-read
