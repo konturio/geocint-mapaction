89b3e3cb6c121cf69d0736bc775d76115d48f9bb
lgudyma <lgudyma@kontur.io>
2022-12-21 15:41:27 +0200

    fixing errors after testing

--- scripts/mapaction_export.sh
+++ scripts/mapaction_export.sh
@@ -13,7 +13,7 @@ OUTDIR="data/out/country_extractions/${base_name%.*}"
 # generate directories for export
 while IFS="," read -r dir_name
 do
-    mkdir -p $dir_name
+    mkdir -p $OUTDIR/$dir_name
 done < <( psql -t -A -F , -c "select a.dir_name from gis.mapaction_directories as a, (select ma_category from ${mapaction_table_name} group by 1) as b where dir_name ~* ma_category")
 
 # generate layernames for export
@@ -31,10 +31,10 @@ do
     # $dir_name is directory according to category
     # generate sql string for ogr, if there are additional columns for layer
     sql=$(psql -t -A -F , -c "select mapaction_data_export('${mapaction_table_name}', '${country_code}', '${ma_category}', '${ma_theme}', '${ma_tag}', '${feature_type}');")
-    ogr2ogr -f "ESRI Shapefile" $OUTDIR$dir_name/$output.shp PG:"dbname=gis" -sql "${sql}" -lco ENCODING=UTF8
-    rm -f $OUTDIR$dir_name/$output.geojson
-    ogr2ogr -f "GeoJSON" $OUTDIR$output.geojson PG:"dbname=gis" -sql "${sql}" -lco WRITE_NAME=NO
-done < <( psql -t -A -F , -c "SELECT country_code, ma_category, ma_theme, feature_type, ma_tag, dir_name  FROM ${mapaction_table_name} mapaction_directories where dir_name ~* ma_category group by 1,2,3,4,5,6")
+    ogr2ogr -f "ESRI Shapefile" $OUTDIR/$dir_name/$output.shp PG:"dbname=gis" -sql "${sql}" -lco ENCODING=UTF8
+    rm -f $OUTDIR/$dir_name/$output.geojson
+    ogr2ogr -f "GeoJSON" $OUTDIR/$dir_name/$output.geojson PG:"dbname=gis" -sql "${sql}" -lco WRITE_NAME=NO
+done < <( psql -t -A -F , -c "SELECT country_code, ma_category, ma_theme, feature_type, ma_tag, dir_name FROM ${mapaction_table_name}, mapaction_directories where dir_name ~* ma_category group by 1,2,3,4,5,6")
 
 # delete empty directories if exists
 find $OUTDIR -type d -empty -delete
