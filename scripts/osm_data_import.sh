1dc64ec13f72661a67ee4f0fd70db73c7d2f1e8c
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-01-04 14:16:12 +0300

    14093 add -1 psql flag to run osm import in single transaction

--- scripts/osm_data_import.sh
+++ scripts/osm_data_import.sh
@@ -9,5 +9,5 @@ base_name=$(basename "$1")
 prefix_name="osm_${base_name%.*}"
 input="data/mid/mapaction/${base_name/json/pbf}"
 psql -1 -f tables/osm_data_table.sql -v tablename=${prefix_name}
-osmium export -c static_data/mapaction_osmium.config.json -f pg $input -v --progress | psql -c "copy $prefix_name from stdin;"
+osmium export -c static_data/mapaction_osmium.config.json -f pg $input -v --progress | psql -c -1 "copy $prefix_name from stdin;"
 psql -1 -c "create index on $prefix_name using gin (tags);"
