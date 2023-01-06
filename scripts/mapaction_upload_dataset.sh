32cdbc7c7711151dc92ce269cb94765f9566b616
Kirill "Zkir <mekillot@yandex.ru>
2023-01-06 14:55:16 +0300

    Path to dataset has been fixed.

--- scripts/mapaction_upload_dataset.sh
+++ scripts/mapaction_upload_dataset.sh
@@ -19,8 +19,8 @@ echo "ckan_dataset_description_json_path $ckan_dataset_description_json_path"
 
 python scripts/build_ckan_dataset_description.py $shp_path > $ckan_dataset_description_json_path
 
-aws s3 cp $output_shp_zip_path $CKAN_DATA_S3_URL$output_shp_zip_path --acl public-read
-aws s3 cp $output_geojson_zip_path $CKAN_DATA_S3_URL$output_geojson_zip_path --acl public-read
+aws s3 cp $output_shp_zip_path $CKAN_DATA_S3_URL$(basename $shp_path).zip --acl public-read
+aws s3 cp $output_geojson_zip_path $CKAN_DATA_S3_URL$(basename $geojson_path).zip --acl public-read
 
 set +e
 curl -H "Content-Type: application/json" -d @$ckan_dataset_description_json_path -H "Authorization: $CKAN_API_KEY" -X POST "$CKAN_BASE_URL/api/3/action/package_create"
