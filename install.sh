6161e79e31b63490a0e67d08762fcc4038b3b9e3
isaricicek <isaricicek@kontur.io>
2023-01-25 18:12:21 +0300

    14392 created symbolic links for completeness test

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 set -e
 
 sudo apt-get install -y curl awscli
+
+ln -s ${GEOCINT_WORK_DIRECTORY}/completeness_report/ ${GEOCINT_WORK_DIRECTORY}/public_html/completeness_report/
+ln -s ${GEOCINT_WORK_DIRECTORY}/report/ ${GEOCINT_WORK_DIRECTORY}/public_html/report/
