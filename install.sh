25e393bff244dc9f8a1885616308dc9aa7df4485
isaricicek <isaricicek@kontur.io>
2023-01-26 14:53:30 +0300

    14392 change on folder structure

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 
 sudo apt-get install -y curl awscli
 
-ln -s ${GEOCINT_WORK_DIRECTORY}/completeness_report/ ${GEOCINT_WORK_DIRECTORY}/public_html/completeness_report/
-ln -s ${GEOCINT_WORK_DIRECTORY}/report/ ${GEOCINT_WORK_DIRECTORY}/public_html/report/
+ln -s ${GEOCINT_WORK_DIRECTORY}/geocint/completeness_report ${GEOCINT_WORK_DIRECTORY}/public_html/completeness_report
+ln -s ${GEOCINT_WORK_DIRECTORY}/geocint/report ${GEOCINT_WORK_DIRECTORY}/public_html/report
