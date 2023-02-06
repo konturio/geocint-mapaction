09ff0ac0dbbef3c4021646a1cf66010ed89acf73
lgudyma <lgudyma@kontur.io>
2023-02-06 16:10:17 +0200

    fixing variable name

--- scripts/mapaction_upload_dataset.sh
+++ scripts/mapaction_upload_dataset.sh
@@ -9,24 +9,24 @@ set -e
 filepath=$1
 filetype=$2
 
-output_shp_zip_path="data/out/country_extractions/$(basename $filepath).zip"
+output_zip_path="data/out/country_extractions/$(basename $filepath).zip"
 
 if [ $filetype = "shp" ]; then
     # include only related to shp(cpg, dbf, prj, shp, shx) and txt
-    zip -r -j $output_shp_zip_path "${filepath%.*}"* -x "*.geojson" "*.tif"
+    zip -r -j $output_zip_path "${filepath%.*}"* -x "*.geojson" "*.tif"
  elif [ $filetype = "geojson" ]; then
     # include only geojson and txt 
-    zip -r -j $output_shp_zip_path "${filepath%.*}"* -i "*.geojson" "*.txt"
+    zip -r -j $output_zip_path "${filepath%.*}"* -i "*.geojson" "*.txt"
  elif [ $filetype = "tif" ]; then
     # include only tif
-    zip -r -j $output_shp_zip_path "${filepath%.*}"* -i "*.tif"
+    zip -r -j $output_zip_path "${filepath%.*}"* -i "*.tif"
 fi
 
 ckan_dataset_description_json_path="data/out/country_extractions/$(basename $filepath)_ckan.json"
 
 python scripts/build_ckan_dataset_description.py $filepath > $ckan_dataset_description_json_path
 
-aws s3 cp $output_shp_zip_path $CKAN_DATA_S3_URL$(basename $filepath).zip --acl public-read
+aws s3 cp $output_zip_path $CKAN_DATA_S3_URL$(basename $filepath).zip --acl public-read
 
 set +e
 curl -H "Content-Type: application/json" -d @$ckan_dataset_description_json_path -H "Authorization: $CKAN_API_KEY" -X POST "$CKAN_BASE_URL/api/3/action/package_create"
