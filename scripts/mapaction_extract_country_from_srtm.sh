d7cab00911ac0e1dcd137449569401d4a1557fe4
lgudyma <lgudyma@kontur.io>
2023-02-07 16:01:59 +0200

    14462-include-elevation-data-in-the-pipeline

--- scripts/mapaction_extract_country_from_srtm.sh
+++ scripts/mapaction_extract_country_from_srtm.sh
@@ -6,8 +6,11 @@ set -e
 
 country_geojson_filename=$(basename "$1")
 country_code="${country_geojson_filename%%.*}"
+outdir="data/out/country_extractions/${country_code}/211_elev/"
+
+mkdir -p $outdir
 
 # clip with country polygon
-gdalwarp -of GTIFF -cutline static_data/countries/$1 -crop_to_cutline -overwrite data/mid/srtm/srtm.vrt "data/out/country_extractions/${country_code}/211_elev/${country_code}_elev_dem_ras_s0_srtm_pp_elevation30m.tif"
+gdalwarp -of GTIFF -cutline static_data/countries/$1 -crop_to_cutline -overwrite data/mid/srtm/srtm.vrt "${outdir}/${country_code}_elev_dem_ras_s0_srtm_pp_elevation30m.tif"
 
 exit 0
