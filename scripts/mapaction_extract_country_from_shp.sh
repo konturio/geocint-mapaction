1c8b36f17e1b3dea83fbb282ebea13b8d90a2e1a
lgudyma <lgudyma@kontur.io>
2023-02-10 15:41:23 +0200

    14737-healthsites-processing-is-too-slow

--- scripts/mapaction_extract_country_from_shp.sh
+++ scripts/mapaction_extract_country_from_shp.sh
@@ -11,7 +11,11 @@ output_shp_name="$output_name.shp"
 output_geojson_name="$output_name.geojson"
 
 mkdir -p "$(dirname $output_shp_name)"
-ogr2ogr -clipsrc static_data/countries/$country_geojson_filename -skipfailures $output_geojson_name $input_shp_name
+
+# try to fasten clipping
+# getting spatial extent from country_geojson_filename
+spatial_extent=$(ogrinfo -so -al static_data/countries/$country_geojson_filename | grep "Extent: " | sed 's/Extent: //g' |sed 's/[\(\),-]//g')
+ogr2ogr -spat $spatial_extent -clipsrc static_data/countries/$country_geojson_filename -skipfailures $output_geojson_name $input_shp_name
 
 # if number of features = 0 then delete geojson and do not create .shp
 res=$(ogrinfo -so -al $output_geojson_name | grep "Feature Count:" | sed 's/Feature Count: //g')
