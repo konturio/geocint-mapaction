f50cf541c64f90ce87a005d57378499f238a4657
lgudyma <lgudyma@kontur.io>
2023-02-15 19:11:32 +0200

    adding value to ma_dictionaries.py

--- scripts/ma_dictionaries.py
+++ scripts/ma_dictionaries.py
@@ -366,6 +366,7 @@ FeatureSource = {
     "gist": "Geographic Information Support Team",
     "ge": "Google Earth",
     "geobnd": "geoboundaries.org",
+    "gmted250": "Global Multi-resolution Terrain Elevation Data 2010",
     "googleearth": "Google Earth",
     "govd": "Government - regional",
     "govl": "Government - local",
